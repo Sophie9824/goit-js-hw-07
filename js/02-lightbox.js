4e83d9565e5111b5e3bdb2a4a7f9b772774e3b1c
Sofiia Novotarska-Kermen <113894293+Sophie9824@users.noreply.github.com>
2023-02-02 10:56:22 +0200

    add hw_07

---
+++ js/02-lightbox.js
@@ -0,0 +1,35 @@
+import { galleryItems } from './gallery-items.js';
+
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+
+const cardsMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+
+
+
+function createGalleryMarkup(galleryItems) {
+
+   return galleryItems.map(({ preview, original, description }) => {
+     return `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+     `;
+   })
+     .join('');
+}
+
+var lightbox = new SimpleLightbox('.gallery a', { 
+    
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+
+ });
+
+
+
